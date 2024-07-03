95f504d654469384eebad07f58f389ce037a7f30
sjayawickrama <54890107+sjayawickrama@users.noreply.github.com>
2024-07-03 20:53:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ mongoose
 	.then(() => console.log("Connected to MongoDB"))
 	.catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => {
 	console.log(`Server is up and runnig on : ${PORT} 🚀🚀🚀`);
